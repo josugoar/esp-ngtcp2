2db69df82881eecedfa4b7028df465c91364ac0d
Josu <56826352+josugoar@users.noreply.github.com>
2025-01-11 04:40:38 +0100

    Update wolfssl submodule to latest commit and add configuration header for build options

---
+++ ngtcp2_crypto_wolfssl/port/private_include/config.h
@@ -0,0 +1,25 @@
+#ifndef __HAVE_CONFIG_H_
+#define __HAVE_CONFIG_H_
+
+/* Define to 1 to enable debug output. */
+#undef DEBUGBUILD
+
+/* Define to 1 if you have the <arpa/inet.h> header file. */
+#define HAVE_ARPA_INET_H 1
+
+/* Define to 1 if you have the <netinet/in.h> header file. */
+#define HAVE_NETINET_IN_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <endian.h> header file. */
+#define HAVE_ENDIAN_H 1
+
+/* Define to 1 if you have the `be64toh' function, otherwise 0. */
+#define HAVE_DECL_BE64TOH 1
+
+/* Define to 1 if you have the `explicit_bzero' function. */
+#define HAVE_EXPLICIT_BZERO 1
+
+#endif // __HAVE_CONFIG_H_
